99be4449e74b66c55930be13f8fa1efb862c6753
Nemo21 <saggubableen9@gmail.com>
2023-08-18 23:37:24 +0530

    Added Header Section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
       <h1>Halo frnds!!!!</h1>
+      <Header />
     </div>
   );
 }
