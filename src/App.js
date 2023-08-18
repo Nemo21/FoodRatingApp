6171a601e78546fbf11e096497ba0fe4ababdd9a
Inderpreet02 <inder192002@gmail.com>
2023-08-18 23:44:31 +0530

    Footer component created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Footer } from './components';
 
 function App() {
   return (
     <div className="App">
       <h1>Halo frnds!!!!</h1>
+      <Footer/>
     </div>
   );
 }
